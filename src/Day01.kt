d8c4b94c28a513abfbbad13e7d7affd5e773ed9b
Thiago Almeida <thiagopk@gmail.com>
2022-12-02 18:33:09 -0300

    feat: solve aoc day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
+fun getElves(input: List<String>):List<Int> {
+    return input
+        .joinToString { it.ifBlank { "*" } }
+        .split('*')
+        .map { it.split(',') }
+        .map { getIntList(it) }
+        .map { it.sum() }
+}
+
+fun getIntList(strList: List<String>): List<Int> {
+    return strList
+        .map { it.trim() }
+        .filter { it.isNotEmpty() }
+        .map { it.toInt() }
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return getElves(input)
+            .max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getElves(input)
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
