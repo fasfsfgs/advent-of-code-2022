0a39396349b4c3ca320a00d70059407a1eccc3ff
Thiago Almeida <thiagopk@gmail.com>
2022-12-11 16:15:36 -0300

    part: solve day 9 part 2 (visualization commented)

--- src/Day09.kt
+++ src/Day09.kt
@@ -18,8 +18,17 @@ fun main() {
         val headPath = input.toMotions()
             .fold(listOf(initialHead)) { acc, motion -> acc.plus(acc.last().move(motion)) }
 
+//        val paths = mutableListOf<List<Knot>>()
+
         val tailPath = List(9) { it }
-            .fold(headPath) { acc, _ -> acc.follower() }
+            .fold(headPath) { acc, _ ->
+                val follower = acc.follower()
+//                paths.add(follower)
+                follower
+            }
+
+//        paths.add(0, headPath)
+//        visualize(paths)
 
         return tailPath
             .distinct()
@@ -35,7 +44,7 @@ fun main() {
 
     val input = readInput("Day09")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 fun List<String>.toMotions(): List<String> = flatMap {
@@ -68,11 +77,44 @@ data class Knot(val x: Int = 0, val y: Int = 0) {
 
         abs(head.x - x) == 1 && head.y == y + 2 -> head.copy(y = y + 1)
         abs(head.x - x) == 1 && head.y == y - 2 -> head.copy(y = y - 1)
+        abs(head.y - y) == 1 && head.x == x + 2 -> head.copy(x = x + 1)
+        abs(head.y - y) == 1 && head.x == x - 2 -> head.copy(x = x - 1)
 
-        head.x == x + 2 -> head.copy(x = x + 1)
-        head.x == x - 2 -> head.copy(x = x - 1)
+        head.x == x + 2 && head.y == y + 2 -> copy(x = x + 1, y = y + 1)
+        head.x == x + 2 && head.y == y - 2 -> copy(x = x + 1, y = y - 1)
+        head.x == x - 2 && head.y == y + 2 -> copy(x = x - 1, y = y + 1)
+        head.x == x - 2 && head.y == y - 2 -> copy(x = x - 1, y = y - 1)
 
         else -> error("$this can't follow $head")
     }
 
+}
+
+fun visualize(paths: List<List<Knot>>) {
+    val numberOfMotions = paths.first().size
+
+    val flattenKnots = paths.flatten()
+    val maxX = flattenKnots.map { it.x }.max()
+    val maxY = flattenKnots.map { it.y }.max()
+
+    for (i in 0 until numberOfMotions) {
+        val state = paths.map { it[i] }
+        visualize(state, maxX, maxY, i)
+    }
+}
+
+fun visualize(state: List<Knot>, maxX: Int, maxY: Int, iteration: Int = 0) {
+    println("ITERATION $iteration")
+    val visualArray = MutableList(maxY + 1) { MutableList(maxX + 1) { "." } }
+
+    for (i in state.lastIndex downTo 0) {
+        val knot = state[i]
+        visualArray[knot.y][knot.x] = i.toString()
+    }
+
+    visualArray
+        .reversed()
+        .forEach { println(it) }
+
+    println()
 }
