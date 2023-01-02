1f4cf406780aa45fcf0d554aa559141fa11dc2e7
Thiago Almeida <thiagopk@gmail.com>
2023-01-02 17:57:46 -0300

    feat: solve aoc day 16 part 2 test

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,22 +1,146 @@
 fun main() {
     fun part1(input: List<String>): Int {
         val valves = input.parseReport()
-
-        val bestPath = findBestPath(valves)
-        return bestPath.cumulativePressure
+        return findBestPath(valves).cumulativePressure
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val valves = input.parseReport()
+        return findBestPathInPair(valves).cumulativePressure
     }
 
     val testInput = readInput("Day16_test")
     check(part1(testInput) == 1651)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 1707)
 
     val input = readInput("Day16")
     println(part1(input))
 //    println(part2(input))
+}
+
+fun findBestPathInPair(valves: List<Valve>): PathInPair {
+    val start = valves.find { it.name == "AA" } ?: error("start not found")
+    val closedValves = valves.filter { it.flow > 0 }
+    val firstPath = PathInPair(start, start, closedValves, 26)
+    val possiblePaths = mutableListOf(firstPath)
+    var bestPath = firstPath
+
+    while (possiblePaths.isNotEmpty()) {
+        val pathToWorkOn = possiblePaths.removeLast()
+
+        if (!isItPossibleToTopBestPathInPair(bestPath, pathToWorkOn)) continue
+
+        val nextPossiblePaths = pathToWorkOn.nextSteps(valves)
+
+        val bestCompletePath = nextPossiblePaths
+            .filter { it.cumulativePressure > bestPath.cumulativePressure }
+            .maxByOrNull { it.cumulativePressure }
+
+        if (bestCompletePath != null) bestPath = bestCompletePath
+
+        possiblePaths.addAll(nextPossiblePaths)
+    }
+
+    return bestPath
+}
+
+fun isItPossibleToTopBestPathInPair(bestPath: PathInPair, path: PathInPair): Boolean {
+    val theoreticalCumulativePressureOfClosedValves = path.valvesToOpen.asSequence()
+        .sortedByDescending { it.flow }
+        .chunked(2)
+        .mapIndexed { index, valves ->
+            if (valves.size == 1)
+                (path.timeLeft - (index + 1)) * valves[0].flow
+            else
+                (path.timeLeft - (index + 1)) * valves[0].flow + (path.timeLeft - (index + 1)) * valves[1].flow
+        }
+        .filter { it > 0 }
+        .sum()
+
+    return (path.cumulativePressure + theoreticalCumulativePressureOfClosedValves) > bestPath.cumulativePressure
+}
+
+data class PathInPair(
+    val pos1: Valve,
+    val pos2: Valve,
+    val valvesToOpen: List<Valve>,
+    val timeLeft: Int,
+    val cumulativePressure: Int = 0,
+    val visitedValvesWithPressure: Map<Valve, Int> = mapOf(pos1 to cumulativePressure)
+)
+
+fun PathInPair.nextSteps(valves: List<Valve>): List<PathInPair> {
+    if (valvesToOpen.isEmpty() || timeLeft == 0) return listOf()
+    val timeLeft = timeLeft - 1
+
+    val canOpenPos1 = valvesToOpen.any { it == pos1 }
+    val canOpenPos2 = pos1 != pos2 && valvesToOpen.any { it == pos2 }
+
+    val newPathsNoValvesOpen = pos1.destinationIds
+        .flatMap { pos1Dest -> pos2.destinationIds.map { Pair(pos1Dest, it) } }
+        .map { Pair(valves.findByName(it.first), valves.findByName(it.second)) }
+        .filter { visitedValvesWithPressure[it.first] != cumulativePressure && visitedValvesWithPressure[it.second] != cumulativePressure }
+        .map {
+            copy(
+                pos1 = it.first, pos2 = it.second,
+                timeLeft = timeLeft,
+                visitedValvesWithPressure = visitedValvesWithPressure + (it.first to cumulativePressure) + (it.second to cumulativePressure)
+            )
+        }
+
+    if (!canOpenPos1 && !canOpenPos2 && pos1 == pos2)
+        return newPathsNoValvesOpen.distinctBy { setOf(it.pos1, it.pos2) }
+
+    if (!canOpenPos1 && !canOpenPos2)
+        return newPathsNoValvesOpen
+
+    var nextSteps = newPathsNoValvesOpen
+
+    if (canOpenPos1) {
+        val cumulativePressure = cumulativePressure + (pos1.flow * timeLeft)
+        val newPathsOnlyValvePos1Open = pos2.destinationIds
+            .map { valves.findByName(it) }
+            .map {
+                copy(
+                    pos2 = it,
+                    valvesToOpen = valvesToOpen - pos1,
+                    timeLeft = timeLeft,
+                    cumulativePressure = cumulativePressure,
+                    visitedValvesWithPressure = visitedValvesWithPressure + (pos1 to cumulativePressure) + (pos2 to cumulativePressure)
+                )
+            }
+        nextSteps = nextSteps + newPathsOnlyValvePos1Open
+    }
+
+    if (canOpenPos2) {
+        val cumulativePressure = cumulativePressure + (pos2.flow * timeLeft)
+        val newPathsOnlyValvePos2Open = pos1.destinationIds
+            .map { valves.findByName(it) }
+            .map {
+                copy(
+                    pos1 = it,
+                    valvesToOpen = valvesToOpen - pos2,
+                    timeLeft = timeLeft,
+                    cumulativePressure = cumulativePressure,
+                    visitedValvesWithPressure = visitedValvesWithPressure + (pos1 to cumulativePressure) + (pos2 to cumulativePressure)
+                )
+            }
+        nextSteps = nextSteps + newPathsOnlyValvePos2Open
+    }
+
+    if (canOpenPos1 && canOpenPos2) {
+        val cumulativePressure = cumulativePressure + (pos1.flow * timeLeft) + (pos2.flow * timeLeft)
+        val bothPosOpen = copy(
+            valvesToOpen = valvesToOpen - pos1 - pos2,
+            timeLeft = timeLeft,
+            cumulativePressure = cumulativePressure,
+            visitedValvesWithPressure = visitedValvesWithPressure + (pos1 to cumulativePressure) + (pos2 to cumulativePressure)
+        )
+
+        nextSteps = nextSteps + bothPosOpen
+    }
+
+    return nextSteps
 }
 
 fun findBestPath(valves: List<Valve>): Path {
@@ -65,7 +189,6 @@ data class Path(
 
 fun Path.nextSteps(valves: List<Valve>): List<Path> {
     if (valvesToOpen.isEmpty() || timeLeft == 0) return listOf()
-
     val timeLeft = timeLeft - 1
 
     val openedPath = if (valvesToOpen.contains(pos)) {
